bd2123dfee0f069858f59d38b533b274aad8eb8e
WagaoFC <wagnerferreira_07@hotmail.com>
2022-08-18 18:25:56 -0300

    atualizando data

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -40,7 +40,7 @@ export function Form() {
         <Container>
             <ToastContainer position="top-center" />
             <form onSubmit={sendEmail}>
-                <h1>Lista Society 18/08</h1>
+                <h1>Lista Society 25/08</h1>
                 <input type="text" required placeholder="Nome ou apelido" name="name" />
                 <input type="submit" value="Enviar meu nome consagrado" />
             </form>
